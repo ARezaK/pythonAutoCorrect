66ab15c5d94103cfbf3e1fc677313f945a72a32c
ARezaK <ARezaK@users.noreply.github.com>
2019-05-07 19:11:58 -0400

    more keys

--- globals_.py
+++ globals_.py
@@ -1,1 +1,5 @@
+import os
+
 rules_folder_name = 'rules'
+
+dir_path = os.path.dirname(os.path.abspath(__file__))
---
+++ main_keyboard.py
@@ -0,0 +1,20 @@
+import keyboard
+from globals_ import *
+
+COMBINATIONS = [
+    ['testing1234', 'testingworks']
+]
+#
+for f_ in os.listdir(os.path.join(dir_path, rules_folder_name)):
+    with open(os.path.join(dir_path, rules_folder_name, f_)) as rules:
+        for rule in rules.readlines():
+            if rule[0] != ';':
+                COMBINATIONS.append([rule.split(';')[0], rule.split(';')[1].strip()])
+
+
+for COMBO in COMBINATIONS:
+    keyboard.add_abbreviation(COMBO[0], COMBO[1] + ' ', True, 3)
+
+print('ready')
+while 1:
+    pass
